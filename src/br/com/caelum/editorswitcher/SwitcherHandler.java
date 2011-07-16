6ae4cd7896dbc1b2779ce36794beb63f0c578658
Hugo Corbucci <hugo.corbucci@gmail.com>
2011-07-16 11:58:00 -0300

    Small refactors. Hugo

--- src/br/com/caelum/editorswitcher/SwitcherHandler.java
+++ src/br/com/caelum/editorswitcher/SwitcherHandler.java
@@ -3,9 +3,9 @@ package br.com.caelum.editorswitcher;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IEditorReference;
 import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 
@@ -15,22 +15,22 @@ public class SwitcherHandler extends AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		int index = Integer.parseInt(event.getParameter(PARAMETER));
-		
+
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
 		IWorkbenchPage activePage = window.getActivePage();
-		
+
 		IEditorReference[] references = activePage.getEditorReferences();
 		int editorReference;
-		if (index == 0) {
+		if (index == 0)
 			editorReference = references.length - 1;
-		} else {
+		else
 			editorReference = Math.min(index - 1, references.length - 1);
-		}
 
-		if(editorReference >= 0) {
+		if (editorReference >= 0) {
+			
 			IEditorReference reference = references[editorReference];
-			IWorkbenchPart part = reference.getPart(true);
-			activePage.activate(part);
+			IEditorPart editor = reference.getEditor(true);
+			activePage.activate(editor);
 		}
 
 		return null;
